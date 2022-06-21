b93e595e86caff981a4588958bfa000c7ee8594b
andres <andres.webdev@gmail.com>
2022-06-20 23:01:43 -0500

    Item Collection II + Andres Torres Gomel

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,11 +3,34 @@ import { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { CartContext } from '../context/CartContext'
 import { CartItem } from './CartItem'
+import {addDoc, collection, orderBy} from 'firebase/firestore'
+import { db } from '../services/firebase'
 
 export const Cart = () => {
 
   const { cart ,getQuantity,getTotalPrice} = useContext(CartContext)
   let cantidad = getQuantity();
+
+  const createOrder = () => {
+    const orderObj = {
+      buyer : {
+        name : 'Andres Torres',
+        email : 'asda@gmail.com',
+        phone : 98765423,
+        address : 'Av siempre vivas 1231',
+        comment : 'Cerca de la casa verde'
+      },
+      items : cart,
+      total : getTotalPrice()
+    }
+
+    const collectionRef = collection(db,'orders')
+
+    addDoc(collectionRef,orderObj).then(res => {
+      console.log(`El id de la orden es ${res.id}`)
+    })
+  }
+
   return (
     (cantidad > 0)
     ?
@@ -20,6 +43,7 @@ export const Cart = () => {
         }
       </ul>
       <p>s./{getTotalPrice()}</p>
+      <button onClick={createOrder}>Generar Orden</button>
     </div>
     :
     <div>
